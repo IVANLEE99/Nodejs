b50f50913dec615eb68d807b314597eef67687ca
youngs <lixiang@popicorns.com>
2024-02-22 11:27:17 +0800

    learn:fs.writeFileSync 同步写入

--- fs/1-文件写入.js
+++ fs/1-文件写入.js
@@ -4,12 +4,15 @@
 
 //1、导入fs模块
 const fs = require("fs");
-//2、写入文件
-fs.writeFile("./座右铭.txt", "三人行必有我师", (err) => {
-  //err 写入失败：错误对象 写入成功：null
-  if (err) {
-    console.error("写入失败");
-    return;
-  }
-  console.log("写入成功");
-});
+//2、写入文件 异步
+// fs.writeFile("./座右铭.txt", "三人行必有我师", (err) => {
+//   //err 写入失败：错误对象 写入成功：null
+//   if (err) {
+//     console.error("写入失败");
+//     return;
+//   }
+//   console.log("写入成功");
+// });
+//同步写入
+fs.writeFileSync("./data.txt", "test");
+console.log("同步写入");
