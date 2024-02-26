2a0db95bfbceee581164d65e59bf18ecf08f1488
youngs <lixiang@popicorns.com>
2024-02-26 18:27:13 +0800

    learn:http.createServer 注意事项

--- http/01_创建http服务.js
+++ http/01_创建http服务.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello world !");
+  //   res.end("Hello world !");
+  res.setHeader("content-type", "text/html;charset=utf-8");
+  res.end("世界 你好～");
 });
 
 server.listen(9000, () => {
