b1f11b9bce61f5cb94ed0d5cf53df4582b3dc9b0
youngs <lixiang@popicorns.com>
2024-04-09 20:53:57 +0800

    learn:npm i -g nodemon

--- http/01_创建http服务.js
+++ http/01_创建http服务.js
@@ -3,9 +3,9 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   //   res.end("Hello world !");
   res.setHeader("content-type", "text/html;charset=utf-8");
-  res.end("世界 你好～");
+  res.end("世界 你好～dfdfdsdfsdfsfs");
 });
 
 server.listen(9000, () => {
-  console.log("服务已经启动。。。");
+  console.log("服务已经启动。dfdfdf。。");
 });
