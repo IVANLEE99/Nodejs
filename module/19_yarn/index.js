77398f24e529e27e861ace774fb2103fd68b00cb
youngs <lixiang@popicorns.com>
2024-04-10 14:06:13 +0800

    learn: npm publish package

---
+++ module/19_yarn/index.js
@@ -0,0 +1,6 @@
+const uniq = require("uniq");
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 1];
+console.log(uniq(arr));
+const youngsdream_math = require("youngsdream_math");
+console.log(youngsdream_math.add(1000, 200));
+console.log(youngsdream_math.sub(1000, 200));
