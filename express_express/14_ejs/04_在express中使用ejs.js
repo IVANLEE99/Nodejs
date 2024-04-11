17c78716809f740d16169c33848ce3bce2a53166
youngs <lixiang@popicorns.com>
2024-04-11 15:28:06 +0800

    learn:express set view and view engine

---
+++ express_express/14_ejs/04_在express中使用ejs.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = new express();
+//1、引入模板引擎
+app.set("view engine", "ejs");
+//2、设置模板文件存放位置
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/home", (req, res) => {
+  //3、render 响应
+  let title = "在 express中使用ejs";
+  res.render("home", { title });
+  //   res.send("home 页面");
+});
+app.listen(9000, () => {
+  console.log("app is listening on 9000...");
+});
