d014fc236d49914c72895d150fbcd27478a0ad41
youngs <lixiang@popicorns.com>
2024-04-11 16:31:35 +0800

    learn:express pug

--- express_express/14_ejs/04_在express中使用ejs.js
+++ express_express/14_ejs/04_在express中使用ejs.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = new express();
-//1、引入模板引擎
-app.set("view engine", "ejs");
-//2、设置模板文件存放位置
-app.set("views", path.resolve(__dirname, "./views"));
+// //1、引入模板引擎
+// app.set("view engine", "ejs");
+// //2、设置模板文件存放位置
+// app.set("views", path.resolve(__dirname, "./views/ejs"));
 
+//1、引入模板引擎
+app.set("view engine", "pug");
+//2、设置模板文件存放位置
+app.set("views", path.resolve(__dirname, "./views/pug"));
 app.get("/home", (req, res) => {
   //3、render 响应
   let title = "在 express中使用ejs";
