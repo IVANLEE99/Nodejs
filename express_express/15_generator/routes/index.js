73d234d694ada63eb352643dcef3c092bb1ed05d
youngs <lixiang@popicorns.com>
2024-04-11 17:33:04 +0800

    learn:express upload files with formidable

--- express_express/15_generator/routes/index.js
+++ express_express/15_generator/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const path = require("path");
+// https://www.npmjs.com/package/formidable
+const { formidable } = require("formidable");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
+//
+router.get("/portrait", function (req, res, next) {
+  res.render("portrait", { title: "Express" });
+});
+router.post("/portrait", function (req, res, next) {
+  const form = formidable({
+    uploadDir: path.resolve(__dirname, "../public/images"),
+    keepExtensions: true,
+  });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // res.json({ fields, files });
+    res.render("upload-sucess", {
+      name: fields.name && fields.name[0],
+      portrait: "/images/" + (files.portrait && files.portrait[0].newFilename),
+    });
+  });
+  // res.send("success");
+});
 module.exports = router;
