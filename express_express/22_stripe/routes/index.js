fe3443ce8addbd0e676366137fbc0f876120d6c3
youngs <lixiang@popicorns.com>
2024-04-16 18:37:39 +0800

    demo:stripe

--- express_express/22_stripe/routes/index.js
+++ express_express/22_stripe/routes/index.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var router = express.Router();
-const $publicKey =
-  "pk_test_51LyskKEmZ3YgtBNvqPmx4st6NVJzimkwXUMEIBgpZuSqA89P27zYybWKYN5JnxVfTNBzPFcwmq7v7o91CdRgIbi80001ksPqDE";
-const $privateKey =
-  "sk_test_51LyskKEmZ3YgtBNvs487pMzeq57BDBkgbGQI1n6hSCnPqhJqMq9Ez185AA67C6rHYi4xp8X3Xj1FKixbzZX6wqWf00DSlDWV9O";
+const $publicKey = process.env.PUBLIC_KEY;
+const $privateKey = process.env.PRIVATE_KEY;
 const stripe = require("stripe")($privateKey);
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -21,7 +19,49 @@ router.get("/secret", async (req, res) => {
   res.json({ client_secret: intent.client_secret });
 });
 
+router.get("/secret2", async (req, res) => {
+  try {
+    const intent = await stripe.paymentIntents.create({
+      amount: 6000,
+      currency: "usd",
+      payment_method_types: ["affirm"],
+      payment_method_data: {
+        type: "affirm",
+      },
+      confirm: true,
+      return_url: "http://localhost:9000/checkout2",
+      // automatic_payment_methods: {
+      //   enabled: true,
+      // },
+    }); // ... Fetch or create the PaymentIntent
+    res.json({ client_secret: intent.client_secret, intent });
+  } catch (error) {
+    res.json(JSON.stringify(error));
+  }
+});
+
 router.get("/checkout", function (req, res, next) {
   res.render("checkout", { title: "Express", $publicKey });
+});
+router.get("/checkout2", function (req, res, next) {
+  res.render("checkout2", { title: "Express", $publicKey });
+});
+router.get("/retrieve/:id", async function (req, res, next) {
+  try {
+    const paymentIntent = await stripe.paymentIntents.retrieve(req.params.id);
+    res.json({ ...paymentIntent });
+  } catch (error) {
+    res.json({ ...error });
+  }
+  // res.render("checkout2", { title: "Express", $publicKey });
+});
+router.get("/payment_methods/:id", async function (req, res, next) {
+  try {
+    const paymentMethod = await stripe.paymentMethods.retrieve(req.params.id);
+    res.json({ ...paymentMethod });
+  } catch (error) {
+    res.json({ ...error });
+  }
+  // res.render("checkout2", { title: "Express", $publicKey });
 });
 module.exports = router;
